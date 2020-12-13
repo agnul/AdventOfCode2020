2eb445d13f44feccfeb33f92ea66bb7c021748b6
agnul <suCrabu@gmail.com>
2020-12-13 23:08:41 +0100

    Too slow!

--- day13/day13.py
+++ day13/day13.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 from functools import reduce
-from math import prod
+from itertools import count
+from math import gcd, prod
 from operator import mul, itemgetter
 
 def solve_part_1(departure, buses):
@@ -11,7 +12,8 @@ def solve_part_1(departure, buses):
             idx, min_wait = b, w
     return buses[idx][1] * min_wait
 
-def solve_part_2(buses):
+
+def solve_part_2_slowly(buses):
     equations = sorted(buses, key=itemgetter(1), reverse=True)
     i, x = 1, equations[0][1]
     for r, n in equations[1:]:
@@ -20,10 +22,21 @@ def solve_part_2(buses):
         i+=1
     return x
 
+
+def solve_part_2(buses):
+    n, step = 0, buses[0][1]
+    for d, t in buses[1:]:
+        for n in count(n, step):
+            if (n + d) % t == 0:
+                break
+        step = (step * t) // gcd(step, t)
+    return n
+
+
 if __name__ == "__main__":
     lines = open('test.txt').readlines()
     departure, buses = int(lines[0]), []
-    for i, b in enumerate('67,x,7,59,61'.split(',')):
-    #for i, b in enumerate(lines[1].split(',')):
+    #for i, b in enumerate('67,x,7,59,61'.split(',')):
+    for i, b in enumerate(lines[1].split(',')):
         if b != 'x': buses.append((i, int(b)))
     print(f'{solve_part_1(departure, buses)}, {solve_part_2(buses)}')
